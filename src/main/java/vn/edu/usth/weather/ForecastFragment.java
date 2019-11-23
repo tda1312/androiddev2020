5b44421ebcbe44f7e12b15acad91b3972b50beaa
tda1312 <tda1312@gmail.com>
2019-11-23 11:40:57 +0700

    Modify fragment frame + Modify app color

--- src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -28,7 +28,6 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_forecast, container, false);
-        v.setBackgroundColor(Color.parseColor("#f32b2b"));
         return v;
     }
 
