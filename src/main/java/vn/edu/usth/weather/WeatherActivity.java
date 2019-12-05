ad4561d1d1bdf94367a895db075777855a19069a
tda1312 <tda1312@users.noreply.github.com>
2019-12-05 22:02:20 +0700

    Labwork 11

--- src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,5 +1,7 @@
 package vn.edu.usth.weather;
 
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +11,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class WeatherActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
 
     public void onFragmentInteraction(Uri uri){
         Log.i("Tag", "onFragmentInteraction called");
@@ -38,6 +48,33 @@ public class WeatherActivity extends AppCompatActivity {
         for(int i = 0; i < tabLayout.getTabCount(); i++){
             tabLayout.getTabAt(i).setIcon(tabIcons[i]);
         }
+
+        InputStream is = getResources().openRawResource(R.raw.mylove);
+
+        writeExternal();
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.mylove);
+        mediaPlayer.start();
+
+    }
+
+    private void writeExternal(){
+        String fileName = "mylove.mp3";
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data";
+        File file = new File(filePath + fileName);
+        try {
+            OutputStream fos;
+            InputStream is = getApplicationContext().getResources().openRawResource(R.raw.mylove);
+            file.createNewFile();
+            fos = new FileOutputStream(file);
+            fos.write(is.read());
+            fos.close();
+            is.close();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -58,6 +95,8 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.i("onPause", "Pausing");
+        mediaPlayer.stop();
+        mediaPlayer.release();
     }
 
     @Override
